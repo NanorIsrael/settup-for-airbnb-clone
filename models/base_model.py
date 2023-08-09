b7660e86dad66c895c3819c9180a41e501488f09
nanorisrael <israel.nanor@compozelabs.com>
2023-08-09 02:13:28 +0000

    Resolve module imports

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,8 @@
 """
 from uuid import uuid4
 from datetime import datetime
+from sys import path
+path.append('.')
 from models import storage
 
 class BaseModel:
@@ -19,12 +21,12 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            # storage.new(self)
     
     def save(self):
 
         self.updated_at = datetime.now()
-        storage.save()
+        # storage.save()
 
     def to_dict(self):
         dictionary = {}
@@ -41,6 +43,30 @@ class BaseModel:
         return f"{[self.__class__.__name__]} {(self.id)} {self.__dict__}"
 
 
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+print(my_model.id)
+print(my_model)
+print(type(my_model.created_at))
+print("--")
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+
+print("--")
+my_new_model = BaseModel(**my_model_json)
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
+
+print("--")
+print(my_model is my_new_model)
+
+# storage = FileStorage()
 all_objs = storage.all()
 print("-- Reloaded objects --")
 for obj_id in all_objs.keys():
@@ -52,5 +78,8 @@ my_model = BaseModel()
 my_model.name = "My_First_Model"
 my_model.my_number = 89
 my_model.save()
-print(my_model)
+save_instance = my_model.to_dict()
+# print(save_instance)
+data = storage.new(save_instance)
+print(data)
 
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,27 +1,39 @@
 #!/usr/bin/python3
 import json
-from models.base_model import BaseModel
+from sys import path
+path.append('.')
+
+"""
+a class FileStorage that serializes instances to a 
+JSON file and deserializes JSON file to instances
+"""
+
 
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """returns the dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
-        myKey = f"{obj.__class__.__name__}.{obj.id}"
+        """dictionary - empty but will store all objects by <class name>.id"""
+        myKey = f"{obj.__class__}.{1}"
         FileStorage.__objects[myKey] = obj
+        return FileStorage.__objects
 
-    def save(self):
-        new_dict = {}
-        for key, value in FileStorage.__objects.items():
-            FileStorage.__objects[key] = value.to_dict()
+    # def save(self):
+    #     new_dict = {}
+    #     for key, value in FileStorage.__objects.items():
+    #         FileStorage.__objects[key] = value.to_dict()
 
-            with open(FileStorage.__file_path, "w", encoding="uft-8") as f:
-                json.dump(new_dict, f)
+    #         with open(FileStorage.__file_path, "w", encoding="UTF-8") as f:
+    #             json.dump(new_dict, f)
 
     def reload(self):
+        from models.base_model import BaseModel
+
         bnbClasses = {'BaseModel': BaseModel}
         try:
             with open(FileStorage.__file_path, "r") as f:
